648168938b342d15de98a1e1f92dd278ac2a4f3c
NeakTuB <seruyfs@mail.ru>
2022-02-20 12:53:23 +0300

    fix problem with logout and registration page

--- web/src/main/java/com/web/configuration/WebSecurityConfiguration.java
+++ web/src/main/java/com/web/configuration/WebSecurityConfiguration.java
@@ -26,11 +26,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/registration").not().fullyAuthenticated()
+                .antMatchers("/registration").permitAll()
                 .antMatchers("/resources/**").permitAll()
                 .anyRequest().authenticated().and()
                 .formLogin().loginPage("/login").permitAll().and()
-                .logout().permitAll();
+                .logout().logoutSuccessUrl("/login?logout").permitAll();
     }
 
 }
